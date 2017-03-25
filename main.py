e75dfc7accbdc49ed3638a238158356626512d6e
AdamJOtto <aotto@ualberta.ca>
2017-03-25 15:24:16 -0600

    Sorted the three text files.  They are now ready to be indexed.

--- main.py
+++ main.py
@@ -2,7 +2,4 @@ from xmlReader import *
 
 #Dont put in an incorrect file name or the program will crash
 #data = readXMLFile('wrongFileName.txt')
-readXMLFile('10.txt')
-print("I just read an XML file!")
-print("I just wrote three files")
-
+#readXMLFile('10.txt')
